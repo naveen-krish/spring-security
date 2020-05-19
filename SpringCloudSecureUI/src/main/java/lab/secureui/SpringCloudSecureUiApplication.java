b60347eef476390b4189efdcaebdb3a8dba21a7d
Naveen <naveenkrishiv@gmail.com>
2020-05-19 19:05:37 +0530

    spring security

---
+++ SpringCloudSecureUI/src/main/java/lab/secureui/SpringCloudSecureUiApplication.java
@@ -0,0 +1,12 @@
+package lab.secureui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudSecureUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudSecureUiApplication.class, args);
+	}
+}
